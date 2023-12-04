75890bf0564d58d0a18fba9caa36f21d7ebb3bd7
Xayida83 <lotta.lindberg83@gmail.com>
2023-12-04 16:25:08 +0100

    Show questions is working

---
+++ questions.js
@@ -0,0 +1,88 @@
+// let questions =[ 
+//   {
+//     question: "Does Ross Geller work as a chef?",
+//     asnwers: [
+//       {text: "No", correct: true},
+//       {text: "Yes", correct: false}
+//     ]
+//   },
+//   {
+//     question: "Who is Monica's brother?",
+//     asnwers: [
+//       {text: "Joey", correct: false},
+//       {text: "Chandler", correct: false},
+//       {text: "Ross", correct: true},
+//       {text: "Mike", correct: false}
+//     ]
+//   },
+//   {
+//     question: "Select the characters who have been married during the series. (Choose 2 or 3)",
+//     asnwers: [
+//       {text: "Ross Geller", correct: true},
+//       {text: "Monica Geller", correct: true},
+//       {text: "Rachel Green", correct: false},
+//       {text: "Chandler Bing", correct: true}
+//     ]
+//   },
+//   {
+//     question: "Is Phoebe's alter ego called Regina Phalange?",
+//     asnwers: [
+//       {text: "No", correct: false},
+//       {text: "Yes", correct: true}
+//     ]
+//   },
+//   {
+//     question: "What is Joey Tribbiani's catchphrase?",
+//     asnwers: [
+//       {text: "Oh my God!", correct: false},
+//       {text: "Pivot!", correct: false},
+//       {text: "How you doin'?", correct: true},
+//       {text: "We were on a break!", correct: false}
+//     ]
+//   },
+//   {
+//     question: "Select the seasons in which Monica and Chandler are a couple. (Choose 2 or 3)",
+//     asnwers: [
+//       {text: "Season 4", correct: true},
+//       {text: "Season 5", correct: true},
+//       {text: "Season 6", correct: true},
+//       {text: "Season 7", correct: false}
+//     ]
+//   },
+//   {
+//     question: "Does Chandler Bing work with statistics and analysis at a data assessment firm?",
+//     asnwers: [
+//       {text: "No", correct: true},
+//       {text: "Yes", correct: false}
+//     ]
+//   },
+//   {
+//     question: "What is the name of Ross and Monica's dog when they were kids?",
+//     asnwers: [
+//       {text: "Rex", correct: false},
+//       {text: "Rover", correct: false},
+//       {text: "Chi-Chi", correct: true},
+//       {text: "Fluffy", correct: false}
+//     ]
+//   },
+//   {
+//     question: "Does Ross Geller own an oversized Porsche costume?",
+//     asnwers: [
+//       {text: "No", correct: false},
+//       {text: "Yes", correct: true}
+//     ]
+//   },
+//   {
+//     question: "Select the characters who have worked at Central Perk. (Choose 2 or 3)",
+//     asnwers: [
+//       {text: "Ross Geller", correct: false},
+//       {text: "Pheobe Buffay", correct: false},
+//       {text: "Rachel Green", correct: true},
+//       {text: "Joey Tribbiani", correct: true}
+//     ]
+//   }
+// ];
+
+// // Nej, svaret är False. Chandler Bing arbetar inte med statistik och analys vid en databedömningsfirma. Han har ett kontorsjobb inom datarobotteknik, och hans yrkesroll är ofta ett skämt inom serien.
+
+
---
+++ script.js
@@ -0,0 +1,143 @@
+let questions =[ 
+  {
+    question: "Does Ross Geller work as a chef?",
+    answers: [
+      {text: "No", correct: true},
+      {text: "Yes", correct: false}
+    ]
+  },
+  {
+    question: "Who is Monica's brother?",
+    answers: [
+      {text: "Joey", correct: false},
+      {text: "Chandler", correct: false},
+      {text: "Ross", correct: true},
+      {text: "Mike", correct: false}
+    ]
+  },
+  {
+    question: "Select the characters who have been married during the series. (Choose 2 or 3)",
+    answers: [
+      {text: "Ross Geller", correct: true},
+      {text: "Monica Geller", correct: true},
+      {text: "Rachel Green", correct: false},
+      {text: "Chandler Bing", correct: true}
+    ]
+  },
+  {
+    question: "Is Phoebe's alter ego called Regina Phalange?",
+    answers: [
+      {text: "No", correct: false},
+      {text: "Yes", correct: true}
+    ]
+  },
+  {
+    question: "What is Joey Tribbiani's catchphrase?",
+    answers: [
+      {text: "Oh my God!", correct: false},
+      {text: "Pivot!", correct: false},
+      {text: "How you doin'?", correct: true},
+      {text: "We were on a break!", correct: false}
+    ]
+  },
+  {
+    question: "Select the seasons in which Monica and Chandler are a couple. (Choose 2 or 3)",
+    answers: [
+      {text: "Season 4", correct: true},
+      {text: "Season 5", correct: true},
+      {text: "Season 6", correct: true},
+      {text: "Season 7", correct: false}
+    ]
+  },
+  {
+    question: "Does Chandler Bing work with statistics and analysis at a data assessment firm?",
+    answers: [
+      {text: "No", correct: true},
+      {text: "Yes", correct: false}
+    ]
+  },
+  {
+    question: "What is the name of Ross and Monica's dog when they were kids?",
+    answers: [
+      {text: "Rex", correct: false},
+      {text: "Rover", correct: false},
+      {text: "Chi-Chi", correct: true},
+      {text: "Fluffy", correct: false}
+    ]
+  },
+  {
+    question: "Does Ross Geller own an oversized Porsche costume?",
+    answers: [
+      {text: "No", correct: false},
+      {text: "Yes", correct: true}
+    ]
+  },
+  {
+    question: "Select the characters who have worked at Central Perk. (Choose 2 or 3)",
+    answers: [
+      {text: "Ross Geller", correct: false},
+      {text: "Pheobe Buffay", correct: false},
+      {text: "Rachel Green", correct: true},
+      {text: "Joey Tribbiani", correct: true}
+    ]
+  }
+];
+
+// Nej, svaret är False. Chandler Bing arbetar inte med statistik och analys vid en databedömningsfirma. Han har ett kontorsjobb inom datarobotteknik, och hans yrkesroll är ofta ett skämt inom serien.
+
+
+
+const toggleModeBtn = document.querySelector("#toggleModeBtn");
+const body = document.body;
+//If the class does not exist add dark-mode.
+toggleModeBtn.addEventListener("click", () => {
+  body.classList.toggle('dark-mode'); 
+});
+
+let questionsContainer = document.querySelector(".questionsContainer");
+let nextQuestionBtn = document.querySelector(".nextBtn");
+let currentQuestionIndex = 0;
+//get questions and options and show on schreen
+// displayQuestion();
+
+nextQuestionBtn.addEventListener("click", () => {
+  questionsContainer.innerHTML = "";
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    displayQuestion();
+  } else {
+    alert("End of quiz");
+  }
+});
+
+let displayQuestion = () => {
+  let card = document.createElement("div");
+  card.classList.add("questionCard");
+
+  let questionText = document.createElement("h2");
+  questionText.className = 'question';
+  questionText.innerText = `${questions[currentQuestionIndex].question}`;
+
+  card.appendChild(questionText);
+
+  questions[currentQuestionIndex].answers.forEach(answer => {
+    let options = document.createElement("div");
+    options.className = 'answersOption';
+
+    let option = document.createElement("input");
+    option.type = "checkbox";
+    option.value = answer.text;
+    option.name = "answers";
+
+    let label = document.createElement("label");
+    label.innerText = answer.text;
+    label.setAttribute('for', answer.text);
+
+    options.appendChild(option);
+    options.appendChild(label);
+    card.appendChild(options);
+  });
+
+  questionsContainer.appendChild(card);
+}
+displayQuestion();
