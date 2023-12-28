4a8769ade7d2d918519708b55b91fe3b05cc6a9c
Xayida83 <lotta.lindberg83@gmail.com>
2023-12-28 15:20:06 +0100

    User can change quiz. has buggs.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//import { default as currentQuiz} from './questionsModule.js';
-// * Second quiz
- import { default as currentQuiz} from './elephantQuizModule.js';
+import { default as friendsQuiz} from './questionsModule.js';
+import { default as elephantQuiz} from './elephantQuizModule.js';
 
+let currentQuiz = null;
 let maxScores = 0;
 let userCorrectResponses = [];
 let userWrongResponses = [];
@@ -22,236 +22,252 @@ const getCheckedCheckboxes = (container) => {
   return container.querySelectorAll("input[type='checkbox']:checked");
 };
 
-let header = document.querySelector(".header");
-header.innerText = currentQuiz.name;
-
-let picture = document.querySelector(".quizImage");
-if (currentQuiz.image){
-  let image = document.createElement('img');
-  image.src = currentQuiz.image;
-  image.alt = "Picture of the subject of the quiz";
-  picture.append(image);
-}
-
-// * Set the maxScore. Count the true answers in the quiz and that is the highscore. 
-currentQuiz.questions.forEach((question) => {
-  question.answers.forEach((answer) => {
-    if (answer.correct) {
-      maxScores += 1;
-    }
-  });
-});
-
 // *If the class does not exist add dark-mode.
 toggleModeBtn.addEventListener("click", () => {
   body.classList.toggle('dark-mode');
 });
-// * Start the quiz
-let startButton = document.querySelector(".start");
-startButton.addEventListener("click", () => {
-  startButton.remove();
-  rulesContainer.classList.add("hide");
-  nextQuestionBtn.classList.remove("hide");
-  displayQuestion(currentQuestionIndex);
-});
 
-// * Count the questions and show one at a time
-nextQuestionBtn.addEventListener("click", () => {
-  let checkedBoxes = getCheckedCheckboxes(questionsContainer);
-  
-  if (checkedBoxes.length === 0) {
-    // *If no boxes are checked, display an alert 
-    alert("You must answer the question before proceeding.");
-    return;
+const quizButtons = document.querySelector("#quizButtons");
+//Create a new button in html for the quiz
+const friendsQuizButton = document.querySelector("#friendsQuiz");
+friendsQuizButton.addEventListener("click", () => loadQuiz(friendsQuiz));
+const elephantQuizButton = document.querySelector("#elephantQuiz");
+elephantQuizButton.addEventListener("click", () => loadQuiz(elephantQuiz));
+
+
+function loadQuiz(selectedQuiz) {
+  currentQuiz = selectedQuiz;
+
+  let header = document.querySelector(".header");
+   header.innerText = currentQuiz.name;
+
+  let picture = document.querySelector(".quizImage");
+  picture.innerHTML = "";
+  if (currentQuiz.image){
+  let image = document.createElement('img');
+  image.src = currentQuiz.image;
+  image.alt = "Picture of the subject of the quiz";
+  picture.append(image);
   }
 
-  questionsContainer.innerHTML = "";
-  
-  currentQuestionIndex++;
-  
- // * If and else to see when questions are over
-  currentQuestionIndex < currentQuiz.questions.length ? displayQuestion(currentQuestionIndex) : (showResult(), showTheAnswers(), nextQuestionBtn.classList.add("hide"));
-});
-
-// * To show the questions and it´s answer options.
-let displayQuestion = (index) => {  
-  let card = document.createElement("div");
-  card.className="questionCard";
-
-  let questionText = document.createElement("h2");
-  questionText.className = "question";
-  questionText.innerText = `${currentQuiz.questions[index].question}`;
-
-  card.append(questionText);
-
-  currentQuiz.questions[index].answers.forEach(answer => {
-    let options = document.createElement("div");
-    options.className = "answersOption";
-
-    let option = document.createElement("input");
-    option.type = "checkbox";
-    option.value = answer.text;
-    option.name = "answers";
-    option.id = answer.text;
-
-    // * Add a "change" event listener for each checkbox and save the response
-    option.addEventListener("change", () => {
-      if(option.checked) {
-        option.disabled = true;
+  // * Set the maxScore. Count the true answers in the quiz and that is the highscore. 
+  currentQuiz.questions.forEach((question) => {
+    question.answers.forEach((answer) => {
+      if (answer.correct) {
+        maxScores += 1;
       }
-
-      let checkBox = getCheckedCheckboxes(card);    
-        checkBox.forEach(checkbox => {
-        let answerText = checkbox.value;
-        updateAnswerClass(currentQuestionIndex, answerText);
-      });
     });
-  
-    let label = document.createElement("label");
-    label.innerText = answer.text;
-    label.setAttribute('for', answer.text);
+  });
+    // * Start the quiz
+    let startButton = document.querySelector(".start");
+    startButton.addEventListener("click", () => {
+      startButton.remove();
+      rulesContainer.classList.add("hide");
+      nextQuestionBtn.classList.remove("hide");
+      displayQuestion(currentQuestionIndex);
+      quizButtons.setAttribute("id", "hide");
+    });
 
-    options.append(option);
-    options.append(label);
-    card.append(options);
+
+
+  // * Count the questions and show one at a time
+  nextQuestionBtn.addEventListener("click", () => {
+    let checkedBoxes = getCheckedCheckboxes(questionsContainer);
+    
+    if (checkedBoxes.length === 0) {
+      // *If no boxes are checked, display an alert 
+      alert("You must answer the question before proceeding.");
+      return;
+    }
+
+    questionsContainer.innerHTML = "";
+    
+    currentQuestionIndex++;
+    
+  // * If and else to see when questions are over
+    currentQuestionIndex < currentQuiz.questions.length ? displayQuestion(currentQuestionIndex) : (showResult(), showTheAnswers(), nextQuestionBtn.classList.add("hide"));
   });
 
-  questionsContainer.append(card);
-}
+  // * To show the questions and it´s answer options.
+  let displayQuestion = (index) => {  
+    let card = document.createElement("div");
+    card.className="questionCard";
 
-// * Check question. If right - this happens. else - other things happens
-let updateAnswerClass = (questionIndex, selectedAnswer) => {
-  // Get current question from array 'questions'
-  let currentQuestion = currentQuiz.questions[questionIndex];
-  // Use the find method to search for the selected answer in the current question's answer options
-  let selectedAnswerObject = currentQuestion.answers.find(answer => answer.text === selectedAnswer);
+    let questionText = document.createElement("h2");
+    questionText.className = "question";
+    questionText.innerText = `${currentQuiz.questions[index].question}`;
 
-  if (selectedAnswerObject) {
-    let optionsElements = document.querySelectorAll('.answersOption');
-    
-    optionsElements.forEach(option => {
-      let label = option.querySelector('label');
-      let answerText = label.innerText;
+    card.append(questionText);
 
-      if (answerText === selectedAnswer) {
-        if (selectedAnswerObject.correct && !label.classList.contains("answered-correct")) {
-          label.classList.add("answered-correct");
-          option.classList.add("correct");
-          console.log("Correct");
-          userCorrectResponses.push({ question: currentQuestion.question, selectedAnswer: selectedAnswer });
-          userScore += correctAnswerPoints;
-        } else if (!selectedAnswerObject.correct && !label.classList.contains("answered-wrong")) {
-          label.classList.add("answered-wrong");
-          option.classList.add("wrong");
-          console.log("Incorrect");
-          userWrongResponses.push({ question: currentQuestion.question, selectedAnswer: selectedAnswer });
-          userScore += incorrectAnswerPoints;
-          disableCheckboxes();
+    currentQuiz.questions[index].answers.forEach(answer => {
+      let options = document.createElement("div");
+      options.className = "answersOption";
+
+      let option = document.createElement("input");
+      option.type = "checkbox";
+      option.value = answer.text;
+      option.name = "answers";
+      option.id = answer.text;
+
+      // * Add a "change" event listener for each checkbox and save the response
+      option.addEventListener("change", () => {
+        if(option.checked) {
+          option.disabled = true;
         }
-      }
+
+        let checkBox = getCheckedCheckboxes(card);    
+          checkBox.forEach(checkbox => {
+          let answerText = checkbox.value;
+          updateAnswerClass(currentQuestionIndex, answerText);
+        });
+      });
+    
+      let label = document.createElement("label");
+      label.innerText = answer.text;
+      label.setAttribute('for', answer.text);
+
+      options.append(option);
+      options.append(label);
+      card.append(options);
     });
-  }
-};
 
-
-//  * A div to display the result
-let showResult = () => {
-  questionsContainer.innerHTML = "";
-  
-  picture.classList.add("hide");
-
-  let percentageCorrect = Math.floor((userScore / maxScores) * 100);
-
-  // Determine the result text and color based on the percentage
-  let resultText = "";
-  let resultColor = "";
-
-  if (percentageCorrect < 50) {
-    resultText = "Failed";
-    resultColor = "rgb(209 87 87)";
-  } else if (percentageCorrect >= 50 && percentageCorrect <= 75) {
-    resultText = "Good";
-    resultColor = "#f1a00c";
-  } else {
-    resultText = "Well done!";
-    resultColor = "rgb(104 167 104)";
+    questionsContainer.append(card);
   }
 
-  //The div to display the results
-  let resultDiv = document.createElement('div');
-  resultDiv.className = "userResult";
-  resultDiv.innerHTML = `<h2 style="color: ${resultColor};">Your Result</h2>
-                        <p>Correct Answers: ${userCorrectResponses.length}</p>
-                        <p>Incorrect Answers: ${userWrongResponses.length}</p>
-                        <p style="color: ${resultColor};">Percentage Correct: ${percentageCorrect}%</p>
-                        <p style="color: ${resultColor};"> - ${resultText}</p>
-                        <p>Score: ${userScore}</p>`;
+  // * Check question. If right - this happens. else - other things happens
+  let updateAnswerClass = (questionIndex, selectedAnswer) => {
+    // Get current question from array 'questions'
+    let currentQuestion = currentQuiz.questions[questionIndex];
+    // Use the find method to search for the selected answer in the current question's answer options
+    let selectedAnswerObject = currentQuestion.answers.find(answer => answer.text === selectedAnswer);
 
-  questionsContainer.append(resultDiv);
-  restartBtn.classList.remove("hide");
-};
+    if (selectedAnswerObject) {
+      let optionsElements = document.querySelectorAll('.answersOption');
+      
+      optionsElements.forEach(option => {
+        let label = option.querySelector('label');
+        let answerText = label.innerText;
 
-let showTheAnswers = () => {
-  let correctAnswersDiv = document.createElement('div');
-  correctAnswersDiv.innerHTML = `<h2>Your correct answers</h2>`;
+        if (answerText === selectedAnswer) {
+          if (selectedAnswerObject.correct && !label.classList.contains("answered-correct")) {
+            label.classList.add("answered-correct");
+            option.classList.add("correct");
+            console.log("Correct");
+            userCorrectResponses.push({ question: currentQuestion.question, selectedAnswer: selectedAnswer });
+            userScore += correctAnswerPoints;
+          } else if (!selectedAnswerObject.correct && !label.classList.contains("answered-wrong")) {
+            label.classList.add("answered-wrong");
+            option.classList.add("wrong");
+            console.log("Incorrect");
+            userWrongResponses.push({ question: currentQuestion.question, selectedAnswer: selectedAnswer });
+            userScore += incorrectAnswerPoints;
+            disableCheckboxes();
+          }
+        }
+      });
+    }
+  };
 
-  // * A dictionary where the keys are questions, and the values are arrays of selected answers. 
-  let questionResponses = {};
 
-userCorrectResponses.forEach((response) => {
-  const { question, selectedAnswer } = response;
+  //  * A div to display the result
+  let showResult = () => {
+    questionsContainer.innerHTML = "";
+    
+    picture.classList.add("hide");
 
-  if (!questionResponses[question]) {
-    questionResponses[question] = [];
-  }
+    let percentageCorrect = Math.floor((userScore / maxScores) * 100);
 
-  questionResponses[question].push(selectedAnswer);
-});
+    // Determine the result text and color based on the percentage
+    let resultText = "";
+    let resultColor = "";
 
-for (const question in questionResponses) {
-  let userAnswerDiv = document.createElement('div');
-  userAnswerDiv.className = "response";
+    if (percentageCorrect < 50) {
+      resultText = "Failed";
+      resultColor = "rgb(209 87 87)";
+    } else if (percentageCorrect >= 50 && percentageCorrect <= 75) {
+      resultText = "Good";
+      resultColor = "#f1a00c";
+    } else {
+      resultText = "Well done!";
+      resultColor = "rgb(104 167 104)";
+    }
 
-  let answers = questionResponses[question].join(', '); // Join multiple answers with commas
+    //The div to display the results
+    let resultDiv = document.createElement('div');
+    resultDiv.className = "userResult";
+    resultDiv.innerHTML = `<h2 style="color: ${resultColor};">Your Result</h2>
+                          <p>Correct Answers: ${userCorrectResponses.length}</p>
+                          <p>Incorrect Answers: ${userWrongResponses.length}</p>
+                          <p style="color: ${resultColor};">Percentage Correct: ${percentageCorrect}%</p>
+                          <p style="color: ${resultColor};"> - ${resultText}</p>
+                          <p>Score: ${userScore}</p>`;
 
-  userAnswerDiv.innerHTML = "<p>Q: " + question + "</p><p>A: " + answers + "</p>";
+    questionsContainer.append(resultDiv);
+    restartBtn.classList.remove("hide");
+  };
 
-  correctAnswersDiv.append(userAnswerDiv);
-}
-  let wrongAnswersDiv = document.createElement('div');
-  wrongAnswersDiv.innerHTML = `<h2>Your wrong answers</h2>`;
+  let showTheAnswers = () => {
+    let correctAnswersDiv = document.createElement('div');
+    correctAnswersDiv.innerHTML = `<h2>Your correct answers</h2>`;
 
-  userWrongResponses.forEach((response) => {
+    // * A dictionary where the keys are questions, and the values are arrays of selected answers. 
+    let questionResponses = {};
+
+  userCorrectResponses.forEach((response) => {
+    const { question, selectedAnswer } = response;
+
+    if (!questionResponses[question]) {
+      questionResponses[question] = [];
+    }
+
+    questionResponses[question].push(selectedAnswer);
+  });
+
+  for (const question in questionResponses) {
     let userAnswerDiv = document.createElement('div');
     userAnswerDiv.className = "response";
 
-    userAnswerDiv.innerHTML = `<p>Q: ${response.question}</p>
-                              <p>A: ${response.selectedAnswer}</p>`;
-    wrongAnswersDiv.append(userAnswerDiv);
+    let answers = questionResponses[question].join(', '); // Join multiple answers with commas
+
+    userAnswerDiv.innerHTML = "<p>Q: " + question + "</p><p>A: " + answers + "</p>";
+
+    correctAnswersDiv.append(userAnswerDiv);
+  }
+    let wrongAnswersDiv = document.createElement('div');
+    wrongAnswersDiv.innerHTML = `<h2>Your wrong answers</h2>`;
+
+    userWrongResponses.forEach((response) => {
+      let userAnswerDiv = document.createElement('div');
+      userAnswerDiv.className = "response";
+
+      userAnswerDiv.innerHTML = `<p>Q: ${response.question}</p>
+                                <p>A: ${response.selectedAnswer}</p>`;
+      wrongAnswersDiv.append(userAnswerDiv);
+    });
+
+    questionsContainer.append(correctAnswersDiv);
+    questionsContainer.append(wrongAnswersDiv);
+  }
+
+  let disableCheckboxes = () => {
+    let checkboxes = document.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(checkbox => {
+      checkbox.disabled = true;
+    });
+  }
+
+  restartBtn.addEventListener("click", () => {
+    // * Reset variables and UI state
+    currentQuestionIndex = 0;
+    userCorrectResponses = [];
+    userWrongResponses = [];
+    questionsContainer.innerHTML = "";
+    nextQuestionBtn.classList.remove("hide");
+    restartBtn.classList.add("hide");
+    picture.classList.remove("hide");
+    userScore = 0;
+
+    // * Start the quiz again
+    displayQuestion(currentQuestionIndex);
   });
-
-  questionsContainer.append(correctAnswersDiv);
-  questionsContainer.append(wrongAnswersDiv);
-}
-
-let disableCheckboxes = () => {
-  let checkboxes = document.querySelectorAll("input[type='checkbox']");
-  checkboxes.forEach(checkbox => {
-    checkbox.disabled = true;
-  });
-}
-
-restartBtn.addEventListener("click", () => {
-  // * Reset variables and UI state
-  currentQuestionIndex = 0;
-  userCorrectResponses = [];
-  userWrongResponses = [];
-  questionsContainer.innerHTML = "";
-  nextQuestionBtn.classList.remove("hide");
-  restartBtn.classList.add("hide");
-  picture.classList.remove("hide");
-  userScore = 0;
-
-  // * Start the quiz again
-  displayQuestion(currentQuestionIndex);
-});
+};
